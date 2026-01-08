d322791df772eaf6395338d339c39aa94b4d4bce
KlunnyS <Tadeas.Nevrela@azet.sk>
2026-01-08 12:30:03 +0100

    EE

--- run_app.bat
+++ run_app.bat
@@ -15,14 +15,14 @@ call .venv\Scripts\activate
 
 :: flask check
 
-python -m pip show flask >null 2>&1 || pip install flask
+python -m pip show flask >NUL 2>&1 || pip install flask
 python -m pip show flask-wtf >null 2>&1 || pip install flask-wtf
 python -m pip show flask-sqlalchemy >null 2>&1 || pip install flask-sqlalchemy
 
 
 :: run app
 echo Running flask app
-flask --app main run --host=0.0.0.0 --port=5000 --debug
+flask --app main run --debug
 
 
 :: exiting
